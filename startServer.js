52890e3163a4a5ba8acd329caff6f962a8ace7aa
Nilam Jadhav <99525537+nilamsjadhav@users.noreply.github.com>
2022-07-13 10:03:26 +0530

    Modified startServer.js

--- startServer.js
+++ startServer.js
@@ -1,8 +1,11 @@
 const http = require('http');
+const { parseBodyParams } = require('./parseBodyParams.js');
+const { parseSearchParams } = require('./parseSearchParams.js');
+const { createRouter } = require('./router.js');
 
 const startServer = (port, app) => {
   const server = http.createServer(app);
   server.listen(port, () => console.log(`server is listening on ${port}`));
 };
 
-module.exports = { startServer };
+module.exports = { startServer, parseBodyParams, parseSearchParams, createRouter };
